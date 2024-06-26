33c86feb439968aa4073e31fdc0888db87de1d13
Aeliot-Tm <5785276@gmail.com>
2024-06-26 21:49:21 +0300

    fix: refactor stored MySQL functions definitions
    - add security characteristic
    - add characteristic about the nature of data use

--- src/Service/DefaultFunctionDefinitionsTrait.php
+++ src/Service/DefaultFunctionDefinitionsTrait.php
@@ -19,34 +19,43 @@ trait DefaultFunctionDefinitionsTrait
         return [
             FunctionEnum::DECRYPT => [
                 PlatformEnum::MYSQL => sprintf(
-                    'CREATE FUNCTION %1$s(source_data LONGBLOB) RETURNS LONGTEXT DETERMINISTIC
-                        BEGIN
-                            RETURN AES_DECRYPT(source_data, %2$s());
-                        END;',
+                    'CREATE
+                        FUNCTION %1$s(source_data LONGBLOB) RETURNS LONGTEXT
+                        DETERMINISTIC
+                        SQL SECURITY DEFINER
+                    BEGIN
+                        RETURN AES_DECRYPT(source_data, %2$s());
+                    END;',
                     FunctionEnum::DECRYPT,
                     FunctionEnum::GET_ENCRYPTION_KEY
                 ),
             ],
             FunctionEnum::ENCRYPT => [
                 PlatformEnum::MYSQL => sprintf(
-                    'CREATE FUNCTION %1$s(source_data LONGTEXT) RETURNS LONGBLOB DETERMINISTIC
-                        BEGIN
-                            RETURN AES_ENCRYPT(source_data, %2$s());
-                        END;',
+                    'CREATE
+                        FUNCTION %1$s(source_data LONGTEXT) RETURNS LONGBLOB
+                        DETERMINISTIC
+                        SQL SECURITY DEFINER
+                    BEGIN
+                        RETURN AES_ENCRYPT(source_data, %2$s());
+                    END;',
                     FunctionEnum::ENCRYPT,
                     FunctionEnum::GET_ENCRYPTION_KEY
                 ),
             ],
             FunctionEnum::GET_ENCRYPTION_KEY => [
                 PlatformEnum::MYSQL => sprintf(
-                    'CREATE FUNCTION %1$s() RETURNS TEXT DETERMINISTIC
-                        BEGIN
-                            IF (@encryption_key IS NULL OR LENGTH(@encryption_key) = 0) THEN
-                                SIGNAL SQLSTATE \'%2$s\'
-                                    SET MESSAGE_TEXT = \'Encryption key not found\';
-                            END IF;
-                            RETURN @encryption_key;
-                        END;',
+                    'CREATE
+                        FUNCTION %1$s() RETURNS TEXT
+                        DETERMINISTIC
+                        SQL SECURITY DEFINER
+                    BEGIN
+                        IF (@encryption_key IS NULL OR LENGTH(@encryption_key) = 0) THEN
+                            SIGNAL SQLSTATE \'%2$s\'
+                                SET MESSAGE_TEXT = \'Encryption key not found\';
+                        END IF;
+                        RETURN @encryption_key;
+                    END;',
                     FunctionEnum::GET_ENCRYPTION_KEY,
                     DatabaseErrorEnum::EMPTY_ENCRYPTION_KEY
                 ),

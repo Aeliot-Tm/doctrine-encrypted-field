eaf03ee0628fe3ecfb01f81f7c50d51441c5f348
Aeliot-Tm <5785276@gmail.com>
2024-11-05 23:18:14 +0300

    Fix code style

--- scripts/php-cs-fixer/config.php
+++ scripts/php-cs-fixer/config.php
@@ -1,6 +1,13 @@
 <?php
 
-use Symfony\Component\Finder\SplFileInfo;
+/*
+ * This file is part of the Doctrine Encrypted Field Bundle.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 use Aeliot\PhpCsFixerBaseline\Service\FilterFactory;
 

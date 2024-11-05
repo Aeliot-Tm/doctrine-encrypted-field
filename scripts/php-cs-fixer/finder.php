d7e532afec675f925bd9273411cf7aacb68c82ac
Aeliot-Tm <5785276@gmail.com>
2024-11-05 23:17:10 +0300

    Remove outdated scripts

--- scripts/php-cs-fixer/finder.php
+++ scripts/php-cs-fixer/finder.php
@@ -6,7 +6,4 @@ return $finder = (new PhpCsFixer\Finder())
     ->files()
     ->ignoreVCS(true)
     ->in(dirname(__DIR__, 2))
-    ->exclude(['var', 'vendor'])
-    ->append([
-        dirname(__DIR__, 2) . '/bin/dev/php-cs-fixer-update-baseline',
-    ]);
+    ->exclude(['var', 'vendor']);
